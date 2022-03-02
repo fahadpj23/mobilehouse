fcad91ef89aa5d80d2a638c7a1f815616b717c2b
fahadpj23 <fahadpj23@gmail.com>
2022-03-02 12:12:44 +0530

    chart

--- src/components/admindashboard/dashboard/chartline.js
+++ src/components/admindashboard/dashboard/chartline.js
@@ -7,13 +7,13 @@ const data = {
     datasets: [
       {
         label: 'sales',
-        data: [400, 1000, 1500,2000, 800, 1500,2000,4000],
+        data: [400, 1000, 1500,2000, 3000,1000,2500],
         backgroundColor:	"#AED6F1 ",
         fill: true,
         pointBackgroundColor:"#ffffff",
-        pointBorderColor:"#8884d8",
+        pointBorderColor:"	#0000FF",
         pointBorderWidth:8,
-        pointRadius:8,
+        pointRadius:2,
         tension: 0.4,
         xAxisID:true,
         yAxisID:"ds",
@@ -45,10 +45,10 @@ const data = {
     },
   };
   
-  function ChartLine() {
+  function  ChartLine(props) {
     return (
       <div>
-        <h2 className='text-xl font-semibold'>Total Sales</h2>
+        <h2 className='text-xl font-semibold'>{props.head}</h2>
         <Line className='mt-5' data={data} options={options}/>
       </div>
     );
--- src/components/admindashboard/dashboard/dashboardMain.js
+++ src/components/admindashboard/dashboard/dashboardMain.js
@@ -1,6 +1,10 @@
 import SideNav from "../sideNav"
 import DetailsBox from "./detailBox"
 import ChartLine from "./chartline"
+import { RiRefund2Line } from 'react-icons/ri';
+import { MdShoppingBasket } from 'react-icons/md';
+import { FaTruck } from 'react-icons/fa';
+import { GiCash } from 'react-icons/gi';
 const DashboardMain=()=>{
     return(
         <div className="flex w-full">
@@ -10,25 +14,29 @@ const DashboardMain=()=>{
          
             <div className="w-9/12 lg:w-10/12 overflow-auto p-3 space-y-5">
                 <div className="flex w-full justify-between space-x-4">
-                    <div className="w-full bg-green-500 text-white rounded-xl shadow-lg p-3">
+                    <div className="w-full bg-green-500 text-white rounded-xl shadow-lg p-3 flex items-center justify-center">
+                        <RiRefund2Line className="text-3xl"/>
                         <DetailsBox
                         head="Total Sales"
                         amount="500"
                         />
                     </div> 
-                    <div className="w-full bg-blue-600 text-white rounded-xl shadow-lg p-3">
+                    <div className="w-full bg-blue-600 text-white rounded-xl shadow-lg p-3 flex items-center justify-center">
+                        <MdShoppingBasket className="text-3xl"/>
                         <DetailsBox
                         head="Total Purchase"
                         amount="500"
                         />
                     </div> 
-                    <div className="w-full bg-red-500 text-white rounded-xl shadow-lg p-3">
+                    <div className="w-full bg-red-500 text-white rounded-xl shadow-lg p-3 flex items-center justify-center">
+                        <FaTruck className="text-3xl"/>
                         <DetailsBox
                         head="Total Orders"
                         amount="500"
                         />
                     </div> 
-                    <div className="w-full bg-yellow-400 text-white rounded-xl shadow-lg p-3">
+                    <div className="w-full bg-yellow-400 text-white rounded-xl shadow-lg p-3 flex items-center justify-center">
+                        <GiCash className="text-3xl"/>
                         <DetailsBox
                         head="Total Revenue"
                         amount="500"
@@ -36,7 +44,39 @@ const DashboardMain=()=>{
                     </div> 
                  
                 </div>
-                <ChartLine/>
+                    <div className="flex w-full space-x-6">
+                        <div className="w-6/12">
+                            <ChartLine
+                             head="Total Sales"
+                            />
+                        </div>
+                        <div className="w-6/12">
+                            <ChartLine
+                             head="Total Purchase"
+                            />
+                        </div>
+                        
+                    
+                    </div>
+                    <div className="flex w-full space-x-6">
+                        <div className="w-6/12">
+                            <ChartLine
+                             head="Total Orders"
+                            />
+                        </div>
+                        <div className="w-6/12">
+                            <ChartLine
+                             head="Total Revenue"
+                            />
+                        </div>
+                        
+                    
+                    </div>
+                    
+                
+                   
+               
+               
 
             </div>
     </div>
