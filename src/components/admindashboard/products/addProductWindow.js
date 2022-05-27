b3fe4b24cd258414298127ca78c6f132a4aca91d
fahadpj23 <fahadpj23@gmail.com>
2022-05-27 16:03:53 +0530

    pproduct add

--- src/components/admindashboard/products/addProductWindow.js
+++ src/components/admindashboard/products/addProductWindow.js
@@ -41,36 +41,36 @@ const AddProductWindow=(props)=>{
                     
                     <div className=" text-sm">
                         <label>Name</label>
-                        <input className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1" name="Name" id="Name" defaultValue={product.name}/>
+                        <input className="w-full border-2 border-gray-400  rounded-md py-1 px-1" name="Name" id="Name" defaultValue={product.name}/>
                     </div>
                     <div className=" text-sm">
                         <h1>Purchase Price</h1>
-                        <input  type="number" className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1"  name="purchasePrice" id="purchasePrice" defaultValue={product.purchasePrice}/>
+                        <input  type="number" className="w-full border-2 border-gray-400  rounded-md py-1 px-1"  name="purchasePrice" id="purchasePrice" defaultValue={product.purchasePrice}/>
                     </div>
                     <div className=" text-sm">
                         <h1>Selling Price</h1>
-                        <input  type="number" className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1"  name="sellingPrice" id="sellingPrice" defaultValue={product.sellingPrice}/>
+                        <input  type="number" className="w-full border-2 border-gray-400  rounded-md py-1 px-1"  name="sellingPrice" id="sellingPrice" defaultValue={product.sellingPrice}/>
                     </div>
                     <div className=" text-sm">
                         <h1>Sales Price</h1>
-                        <input  type="number" className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1"  name="salesPrice" id="salesPrice" defaultValue={product.salesPrice}/>
+                        <input  type="number" className="w-full border-2 border-gray-400  rounded-md py-1 px-1"  name="salesPrice" id="salesPrice" defaultValue={product.salesPrice}/>
                     </div>
               
                
                
                      <div className=" text-sm">
                         <h1>MRP</h1>
-                        <input  className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1" name="MRP" id="MRP" defaultValue={product.mrp}/>
+                        <input  className="w-full border-2 border-gray-400  rounded-md py-1 px-1" name="MRP" id="MRP" defaultValue={product.mrp}/>
                     </div>
                     <div className=" text-sm">
                         <h1>Warranty</h1>
-                        <input  type="text" className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1"  name="Warranty" id="Warranty" defaultValue={product.warranty}/>
+                        <input  type="text" className="w-full border-2 border-gray-400  rounded-md py-1 px-1"  name="Warranty" id="Warranty" defaultValue={product.warranty}/>
                     </div>
 
                     <div className=" text-sm">
                         <h1>HSN</h1>
-                        {/* <input  type="number" className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1"  name="GST" id="GST" defaultValue={product.GST}/> */}
-                        <input defaultValue={product.HSN} type="text" list="data" id="HSN" name="HSN" className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1" />
+                        {/* <input  type="number" className="w-full border-2 border-gray-400  rounded-md py-1 px-1"  name="GST" id="GST" defaultValue={product.GST}/> */}
+                        <input defaultValue={product.HSN} type="text" list="data" id="HSN" name="HSN" className="w-full border-2 border-gray-400  rounded-md py-1 px-1" />
 
                         <datalist id="data">
                             {HSN && HSN.Data.map((item, key) =>
@@ -81,7 +81,7 @@ const AddProductWindow=(props)=>{
                     </div>
                     <div className=" text-sm">
                         <h1>Tax</h1>
-                        <select defaultValue={product.Tax} className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1" id="Tax" name="Tax">
+                        <select defaultValue={product.Tax} className="w-full border-2 border-gray-400  rounded-md py-1 px-1" id="Tax" name="Tax">
                             <option value="">--select--</option>
                             <option value="0">GST_0</option>
                             <option value="3">GST_3</option>
@@ -97,12 +97,12 @@ const AddProductWindow=(props)=>{
                   
                     <div className=" text-sm">
                         <h1>Brand</h1>
-                        <input  className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1"  name="Brand" id="Brand" defaultValue={product.Brand}/>
+                        <input  className="w-full border-2 border-gray-400  rounded-md py-1 px-1"  name="Brand" id="Brand" defaultValue={product.Brand}/>
                     </div>
              
                     <div className=" text-sm">  
                         <h1>Max Qty</h1>
-                        <input  className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1"  name="qty" id="qty" defaultValue={product.qty}/>
+                        <input  className="w-full border-2 border-gray-400  rounded-md py-1 px-1"  name="qty" id="qty" defaultValue={product.qty}/>
                     </div>
                     
             
@@ -111,7 +111,7 @@ const AddProductWindow=(props)=>{
                         return(
                             <div key={key} className=" text-sm">
                                 <h1>{item.attributeName}</h1>
-                                <select defaultValue={product[item.attributeName]}  type="select"  className="w-full border-2 border-gray-400 focus:outline-none rounded-md py-1 px-1"  name={item.attributeName} id={item.attributeName}>
+                                <select defaultValue={product[item.attributeName]}  type="select"  className="w-full border-2 border-gray-400  rounded-md py-1 px-1"  name={item.attributeName} id={item.attributeName}>
                                     <option>--select--</option> 
                                     {
                                         item.value.map((item1,key1)=>
@@ -129,7 +129,7 @@ const AddProductWindow=(props)=>{
             <div className=" text-sm mt-4">
                         {/* <h1>Image</h1> */}
                         <div className="flex space-x-2">
-                            <input onChange={(e)=>{uploadimage(e) }} ref={imageref}  type="file" className="w-full hidden focus:outline-none border-2 border-gray-400 rounded-md h-8 px-1"  name="image" id="image" />
+                            <input onChange={(e)=>{uploadimage(e) }} ref={imageref}  type="file" className="w-full hidden  border-2 border-gray-400 rounded-md h-8 px-1"  name="image" id="image" />
 
                             <button type="button" onClick={()=>uploadimageButtonclick(1)} className="  p-2 rounded border border-gray-400">
                                 <img src={props.productImageblob[0] ? props.productImageblob[0] :  "/uploadimage.png"} alt="" className="object-contain h-16 w-16 overflow-hidden" />
@@ -162,8 +162,8 @@ const AddProductWindow=(props)=>{
                     <img src={`http://localhost:9000/images/${product.image}`} alt="" className="object-contain h-48 overflow-hidden "/>
                     } */}
             <div className="flex justify-end  space-x-3 w-full mt-8 ">
-                    <button className="bg-red-500 text-white py-1 font-semibold rounded-md  focus:outline-none px-1 w-3/12">RESET</button>
-                    <button   type="submit"  value="submit" className="bg-blue-500 text-white py-1 focus:outline-none font-semibold rounded-md  px-1 w-3/12">Save</button>
+                    <button className="bg-red-500 text-white py-1 font-semibold rounded-md   px-1 w-3/12">RESET</button>
+                    <button   type="submit"  value="submit" className="bg-blue-500 text-white py-1  font-semibold rounded-md  px-1 w-3/12">Save</button>
             </div>
             
         </form>
