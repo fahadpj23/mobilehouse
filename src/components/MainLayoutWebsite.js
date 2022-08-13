82b4ccb28340c7064f0baca80344aaf489e49889
fahadpj23 <fahadpj23@gmail.com>
2022-08-13 11:52:11 +0530

    whatsapp

--- src/components/MainLayoutWebsite.js
+++ src/components/MainLayoutWebsite.js
@@ -12,7 +12,7 @@ const MainLayoutWebsite=({children})=>{
      
          <main>{children}</main>
       
-             <a className='bg-green-600 rounded-full fixed bottom-10 right-8 p-2 md:p-3 animate-bounce' href={`https://wa.me/+919745286899?text=${document.location.href}`} target="_blank"><h1><SiWhatsapp className='text-white text-2xl'/></h1></a> 
+             <a className='bg-green-600 rounded-full fixed bottom-10 right-8 p-2 md:p-3 animate-bounce' href={`https://wa.me/+918304830868?text=${document.location.href}`} target="_blank"><h1><SiWhatsapp className='text-white text-2xl'/></h1></a> 
 
 
      
